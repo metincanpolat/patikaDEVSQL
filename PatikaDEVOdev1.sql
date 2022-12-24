c35f2b5ae8ad91819111ecc68a74886d67d27710
metincanpolat <115377013+metincanpolat@users.noreply.github.com>
2022-12-25 01:08:49 +0300

    Update PatikaDEVOdev1.sql
    Odev4Added

--- PatikaDEVOdev1.sql
+++ PatikaDEVOdev1.sql
@@ -59,3 +59,27 @@ SELECT * FROM film WHERE length < 50 AND rental_rate <> 2.99 AND rental_rate <> 
  select title from film where title ILIKE '%t%t%t%t%';
  -- 4 --
  select * from film where title like 'C%' and length>90 and rental_rate=2.99;
+ 
+  -- Odev 4--
+ /*
+1-  film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+2- film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+3- film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+4- country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+5- city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+*/
+
+-- 1 --
+select distinct replacement_cost from film;
+
+-- 2 --
+select count(distinct replacement_cost) from film;
+
+-- 3 --
+select count(title) from film where title like 'T%' and rating = 'G';
+
+-- 4 --
+select Count(*) from country where country like '_____';
+
+-- 5 -- 
+select count(*) from city where city ilike '%r';
